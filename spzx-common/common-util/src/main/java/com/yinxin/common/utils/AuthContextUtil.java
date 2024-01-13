4a56bcf9f9e13f28ef235093c9ac2e5d7f2af1d0
YinXin <1975940254@qq.com>
2024-01-13 14:48:16 +0800

    公共工具模块：添加threadlocal

---
+++ spzx-common/common-util/src/main/java/com/yinxin/common/utils/AuthContextUtil.java
@@ -0,0 +1,24 @@
+package com.yinxin.common.utils;
+
+import com.yinxin.spzx.model.entity.system.SysUser;
+
+/**
+ *
+ * @author YinXin
+ * @date 2024-01-13 13:52
+ */
+public class AuthContextUtil {
+    private final static ThreadLocal<SysUser> SYS_USER_THREAD_LOCAL = new ThreadLocal<>();
+
+    public static void set(SysUser sysUser) {
+        SYS_USER_THREAD_LOCAL.set(sysUser);
+    }
+
+    public static SysUser get() {
+        return SYS_USER_THREAD_LOCAL.get();
+    }
+
+    public static void remove() {
+        SYS_USER_THREAD_LOCAL.remove();
+    }
+}
