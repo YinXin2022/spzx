006e179d9ebc85c23183ff46fee68d18d69063bb
YinXin <1975940254@qq.com>
2024-01-14 21:42:41 +0800

    公共工具模块：添加文件和url常量类

---
+++ spzx-common/common-util/src/main/java/com/yinxin/common/contest/FileContest.java
@@ -0,0 +1,16 @@
+package com.yinxin.common.contest;
+
+/**
+ * @author YinXin
+ * @date 2024-01-14 19:58
+ */
+public class FileContest {
+    /**
+     * 文件日期格式
+     */
+    public static final String FILE_DATE_FORMAT = "yyyyMMdd";
+    /**
+     * 文件路径分隔符
+     */
+    public static final String FILE_SEPARATOR = "/";
+}
---
+++ spzx-common/common-util/src/main/java/com/yinxin/common/contest/UrlContest.java
@@ -0,0 +1,9 @@
+package com.yinxin.common.contest;
+
+/**
+ * @author YinXin
+ * @date 2024-01-14 21:08
+ */
+public class UrlContest {
+    public static final String URI_SEPARATOR = "/";
+}
