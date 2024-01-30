4f7b8fc477b538fb8cb37e7b9bf684159c9ddf7c
YinXin <1975940254@qq.com>
2024-01-30 17:28:03 +0800

    dev模块：完成品牌查询条件类

---
+++ spzx-model/src/main/java/com/yinxin/spzx/model/filter/BrandFilter.java
@@ -0,0 +1,12 @@
+package com.yinxin.spzx.model.filter;
+
+import lombok.Data;
+
+/**
+ * @author YinXin
+ * @date 2024-01-30 17:05
+ */
+@Data
+public class BrandFilter {
+    public Long categoryId;
+}
