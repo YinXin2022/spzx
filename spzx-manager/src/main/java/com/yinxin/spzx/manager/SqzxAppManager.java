92cae3bc086deab9613c267704390a51dd4e6575
YinXin <1975940254@qq.com>
2024-02-22 15:09:19 +0800

    后台管理模块：启用定时任务功能

--- spzx-manager/src/main/java/com/yinxin/spzx/manager/SqzxAppManager.java
+++ spzx-manager/src/main/java/com/yinxin/spzx/manager/SqzxAppManager.java
@@ -3,12 +3,14 @@ package com.yinxin.spzx.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author YinXin
  * @date 2024-01-12 18:25
  */
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan("com.yinxin.*")
 public class SqzxAppManager {
     public static void main(String[] args) {
