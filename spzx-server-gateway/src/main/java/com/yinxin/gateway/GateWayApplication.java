dccd80aba7468dc46f7b11e39c9acaa2943539d9
YinXin <1975940254@qq.com>
2024-02-29 16:54:32 +0800

    网关模块：添加网关服务

---
+++ spzx-server-gateway/src/main/java/com/yinxin/gateway/GateWayApplication.java
@@ -0,0 +1,15 @@
+package com.yinxin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YinXin
+ * @date 2024-02-29 16:13
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
