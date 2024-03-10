06459e1afc686e2c984b289ac69815bfaae1e407
YinXin <1975940254@qq.com>
2024-03-10 16:46:31 +0800

    前台服务模块-网关模块：添加redis并配置全局过滤器

--- spzx-server-gateway/src/main/java/com/yinxin/gateway/GateWayApplication.java
+++ spzx-server-gateway/src/main/java/com/yinxin/gateway/GateWayApplication.java
@@ -1,5 +1,6 @@
 package com.yinxin.gateway;
 
+import com.yinxin.common.annotation.EnableRedisCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2024-02-29 16:13
  */
 @SpringBootApplication
+@EnableRedisCache
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
