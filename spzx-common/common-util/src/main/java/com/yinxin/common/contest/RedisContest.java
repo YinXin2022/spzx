89115a30e6c6235ef90a8edde698ebe62027d18d
YinXin <1975940254@qq.com>
2024-03-11 19:21:19 +0800

    公共工具模块：添加redis常量

--- spzx-common/common-util/src/main/java/com/yinxin/common/contest/RedisContest.java
+++ spzx-common/common-util/src/main/java/com/yinxin/common/contest/RedisContest.java
@@ -10,4 +10,5 @@ public class RedisContest {
     public final static String CATEGORY_TREE = "category:tree";
     public final static String PHONE_CODE = "phone:code:";
     public static final String USER_TOKEN = "user:token:";
+    public static final String USER_CART = "user:cart:";
 }
