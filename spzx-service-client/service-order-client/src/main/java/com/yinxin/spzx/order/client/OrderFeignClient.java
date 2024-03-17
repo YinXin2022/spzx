68e2ef4d96212d2ea67c27c18174203c8b0a2e7a
YinXin <1975940254@qq.com>
2024-03-17 09:12:09 +0800

    远程调用模块-order：order模块向外提供接口

---
+++ spzx-service-client/service-order-client/src/main/java/com/yinxin/spzx/order/client/OrderFeignClient.java
@@ -0,0 +1,17 @@
+package com.yinxin.spzx.order.client;
+
+import com.yinxin.spzx.model.entity.order.OrderInfo;
+import com.yinxin.spzx.model.vo.common.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author YinXin
+ * @date 2024-03-17 9:08
+ */
+@FeignClient("service-order")
+public interface OrderFeignClient {
+    @GetMapping("/api/order/orderInfo/auth/getOrderInfoByOrderNo/{orderNo}")
+    Result<OrderInfo> getOrderInfoByOrderNo(@PathVariable String orderNo) ;
+}
