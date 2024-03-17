90c05c6bc814865cd50a6a734665db37a80d71ec
YinXin <1975940254@qq.com>
2024-03-17 08:56:20 +0800

    前台服务模块-支付模块：构建支付模块

---
+++ spzx-service/service-pay/src/main/java/com/yinxin/pay/PayApplication.java
@@ -0,0 +1,15 @@
+package com.yinxin.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YinXin
+ * @date 2024-03-17 8:52
+ */
+@SpringBootApplication
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
